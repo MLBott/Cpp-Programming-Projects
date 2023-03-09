2e6e47e54301ef040ff217eab16fd93bda55aab8
Michael Bottom <mlbmike@gmail.com>
2023-03-08 17:04:40 -0800

    Update A2 project 8.cpp

--- A2/A2 project 8.cpp
+++ A2/A2 project 8.cpp
@@ -7,27 +7,37 @@ And then outputs the matching temp.*/
 
 #include "stdafx.h"
 #include <iostream>
-
 using namespace std;
+
 int main()
 {
-	double fTemp;
-	int cTemp = 100;
-	int i;
-	int matchTemp;
-	for (i = 0; i < 400; i++)
-	{
-		fTemp = ((9.0 / 5.0) * double(cTemp)) + 32.0;
+    // Declare variables
+    double fTemp; // Fahrenheit temperature
+    int cTemp = 100; // Celsius temperature
+    int i; // Counter for the loop
+    int matchTemp; // Variable to hold the matching temperature
 
-		if (cTemp == fTemp)
-		{
-			cout << "Matching temperature is: " << fTemp << endl;
-		}
-		cTemp = cTemp - 1;
-	}
+    // Loop through a range of Celsius temperatures
+    for (i = 0; i < 400; i++)
+    {
+        // Convert the Celsius temperature to Fahrenheit
+        fTemp = ((9.0 / 5.0) * double(cTemp)) + 32.0;
 
-	return 0;
+        // Check if the Celsius temperature is equal to the Fahrenheit temperature
+        if (cTemp == fTemp)
+        {
+            // If they are equal, print out the matching temperature
+            cout << "Matching temperature is: " << fTemp << endl;
+        }
 
+        // Decrease the Celsius temperature for the next iteration of the loop
+        cTemp = cTemp - 1;
+    }
+
+    // End the program
+    return 0;
 }
+
+
 
 
