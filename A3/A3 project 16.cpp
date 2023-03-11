ff9c5380de7155f33f7c10132214dd150bd7e721
Michael Bottom <mlbmike@gmail.com>
2023-03-11 15:53:23 -0800

    Update A3 project 16.cpp

--- A3/A3 project 16.cpp
+++ A3/A3 project 16.cpp
@@ -6,46 +6,56 @@ and find the highest score and the player associated with that score. Those
 found values, highest player score and highest score, are assigned to the function
 formal parameters.*/
 
-#include "stdafx.h"
+
 #include <iostream>
-#include <string>
 #include <fstream>
-
-
+#include <string>
 using namespace std;
 
+// Main function
 int main()
 {
-	string playerName = "";
-	int playerHighScore = 0;
-
-	string getHighScore(string playerName, int playerHighScore);
-
-	getHighScore(playerName, playerHighScore);
-	return 0;
+    // Initialize variables
+    string playerName = "";
+    int playerHighScore = 0;
+    
+    // Call the getHighScore function to get the highest score and player name from the scores.txt file
+    getHighScore(playerName, playerHighScore);
+    
+    // Return 0 to exit the program
+    return 0;
 }
 
+// Function for getting the highest score and player name from the scores.txt file
 string getHighScore(string playerName, int playerHighScore)
 {
-	ifstream infile("scores.txt");
-	string a, b;
-	int highestScore;
-	
-
-	while (infile >> a >> b)
-	{
-		cout << a << endl << b << endl;
-		int highestScore = stoi(b);
-		if (highestScore > playerHighScore)
-		{
-			playerHighScore = highestScore;
-			playerName = a;
-		}
-		
-	}
-	cout << endl << "The highest is " << playerName << " with a score of " << playerHighScore << endl;
-	return 0;
-
+    // Open the scores.txt file for reading
+    ifstream infile("scores.txt");
+    
+    // Initialize variables
+    string a, b;
+    int highestScore;
+    
+    // Read through each line of the file and print the player name and score to the console
+    while (infile >> a >> b)
+    {
+        cout << a << endl << b << endl;
+        
+        // Convert the score from a string to an integer
+        int highestScore = stoi(b);
+        
+        // If the score is higher than the current playerHighScore, update the playerHighScore and playerName
+        if (highestScore > playerHighScore)
+        {
+            playerHighScore = highestScore;
+            playerName = a;
+        }
+    }
+    
+    // Print the highest score and player name to the console
+    cout << endl << "The highest is " << playerName << " with a score of " << playerHighScore << endl;
+    
+    // Return 0 to exit the function (this function doesn't actually return a string, but the function signature requires it)
+    return 0;
 }
-
 
