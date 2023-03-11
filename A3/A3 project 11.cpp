7dd78e703acf84c8d3bcc9d51a93486e02ba5df2
Michael Bottom <mlbmike@gmail.com>
2023-03-11 15:51:38 -0800

    Update A3 project 11.cpp

--- A3/A3 project 11.cpp
+++ A3/A3 project 11.cpp
@@ -6,125 +6,110 @@ takes a turn rolling the six-sided die, attempting to reach 20 without getting a
 of 1, which loses all points for that round. Whichever player makes it to 100 first wins.
 Separate functions are used for the user turn, computer turn, and dice roll.*/
 
-#include "stdafx.h"
 #include <iostream>
 #include <string>
-
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-
+// Main function
 int main()
 {
-	int i;
-	int humanTotalScore = 0;
-	int computerTotalScore = 0;
-	int humanTurn(int humanTotalScore);
-	int computerTurn(int computertotalScore);
+    // Initialize variables
+    int i;
+    int humanTotalScore = 0;
+    int computerTotalScore = 0;
+    
+    // Declare function prototypes
+    int humanTurn(int humanTotalScore);
+    int computerTurn(int computertotalScore);
 
-	for (i = 0; humanTotalScore < 100 || computerTotalScore < 100; i++)
-	{
+    // Continue the game as long as neither player's total score is 100 or more
+    for (i = 0; humanTotalScore < 100 || computerTotalScore < 100; i++)
+    {
+        // Call the humanTurn and computerTurn functions to take their turns
+        humanTotalScore = humanTotalScore + humanTurn(humanTotalScore);
+        computerTotalScore = computerTotalScore + computerTurn(computerTotalScore);
+        
+        // Check if either player has won, and print a message to the console
+        if (humanTotalScore >= 100)
+        {
+            cout << endl;
+            cout << "Winner! Winner! Congratulations." << endl;
+            break;
+        }
+        if (computerTotalScore >= 100)
+        {
+            cout << endl;
+            cout << "Sorry, computer won. Please try again." << endl;
+            break;
+        }
+    }
 
-		humanTotalScore = humanTotalScore + humanTurn(humanTotalScore);
-		computerTotalScore = computerTotalScore + computerTurn(computerTotalScore);
-		if (humanTotalScore >= 100)
-		{
-			cout << endl;
-			cout << "Winner! Winner! Congratulations." << endl;
-			break;
-		}
-		if (computerTotalScore >= 100)
-		{
-			cout << endl;
-			cout << "Sorry, computer won. Please try again." << endl;
-			break;
-		}
-
-	}
-
-	return 0;
+    // Return 0 to exit the program
+    return 0;
 }
 
+// Function for the human player's turn
 int humanTurn(int humanTotalScore)
 {
-	int j;
-	int humanRollTurnTotal = 0;
-	int humanLastTotal = 0;
-	int calculatedRoll = 0;
-	string holdChoice = "NA";
-	int userRoll();
+    // Initialize variables
+    int j;
+    int humanRollTurnTotal = 0;
+    int humanLastTotal = 0;
+    int calculatedRoll = 0;
+    string holdChoice = "NA";
+    
+    // Declare function prototype
+    int userRoll();
 
-	for (j = 0; humanRollTurnTotal < 20; j++)
-	{
-		cout << "Player Rolling... \n";
-		humanLastTotal = humanTotalScore;
-		humanTotalScore = humanTotalScore + userRoll() + 1;
-		calculatedRoll = humanTotalScore - humanLastTotal;
-		cout << "You rolled a ";
-		cout << calculatedRoll;
-		cout << endl;
-		if (calculatedRoll == 1)
-		{
-			cout << "Oh no, you rolled a 1. Turn Lost!" << endl;
-			humanTotalScore = humanTotalScore - 1;
-			return 0;
-		}
-		humanRollTurnTotal = humanRollTurnTotal + calculatedRoll;
-		cout << "Your total score is: ";
-		cout << humanTotalScore;
-		cout << endl;
-		cout << "Would you like to ROLL AGAIN <R> or HOLD <H>? ";
-		cin >> holdChoice;
-		if (holdChoice == "H")
-		{
-			cout << endl << endl << endl;
-			break;
-		}
-		cout << endl;
-
-	}
-	return humanRollTurnTotal;
+    // Continue the turn as long as the humanRollTurnTotal is less than 20
+    for (j = 0; humanRollTurnTotal < 20; j++)
+    {
+        // Roll the dice and update the total score
+        cout << "Player Rolling... \n";
+        humanLastTotal = humanTotalScore;
+        humanTotalScore = humanTotalScore + userRoll() + 1;
+        calculatedRoll = humanTotalScore - humanLastTotal;
+        cout << "You rolled a ";
+        cout << calculatedRoll;
+        cout << endl;
+        
+        // If the player rolls a 1, their turn ends and a message is printed to the console
+        if (calculatedRoll == 1)
+        {
+            cout << "Oh no, you rolled a 1. Turn Lost!" << endl;
+            humanTotalScore = humanTotalScore - 1;
+            return 0;
+        }
+        
+        // Update the humanRollTurnTotal and print the total score to the console
+        humanRollTurnTotal = humanRollTurnTotal + calculatedRoll;
+        cout << "Your total score is: ";
+        cout << humanTotalScore;
+        cout << endl;
+        
+        // Ask the player if they want to roll again or hold, and get their input
+        cout << "Would you like to ROLL AGAIN <R> or HOLD <H>? ";
+        cin >> holdChoice;
+        
+        // If the player chooses to hold, break out of the loop
+        if (holdChoice == "H")
+        {
+            cout << endl << endl << endl;
+            break;
+        }
+        cout << endl;
+    }
+    
+    // Return the humanRollTurnTotal
+    return humanRollTurnTotal;
 }
 
+// Function for the computer player's turn
 int computerTurn(int computerTotalScore)
 {
-	int k;
-	int computerRollTurnTotal = 0;
-	int computerLastTotal = 0;
-	int compCalculatedRoll = 0;
-	int userRoll();
-
-	for (k = 0; computerRollTurnTotal < 20; k++)
-	{
-		cout << endl;
-		cout << "Computer Rolling... \n";
-		computerLastTotal = computerTotalScore;
-		computerTotalScore = computerTotalScore + userRoll() + 1;
-		compCalculatedRoll = computerTotalScore - computerLastTotal;
-		cout << "Computer rolled a ";
-		cout << compCalculatedRoll;
-		cout << endl;
-		if (compCalculatedRoll == 1)
-		{
-			cout << "Oh no, the computer rolled a 1. It loses it's turn!" << endl;
-			computerTotalScore = computerTotalScore - 1;
-			return 0;
-		}
-		computerRollTurnTotal = computerRollTurnTotal + compCalculatedRoll;
-		cout << "The computer's total score is: ";
-		cout << computerTotalScore;
-		cout << endl;
-
-
-	}
-	return computerRollTurnTotal;
-}
-
-int userRoll()
-{
-	int roll;
-	roll = rand() % 6;
-	return roll;
-}
-
-
+    // Initialize variables
+    int k;
+    int computerRollTurnTotal = 0;
+    int computer 
