1d8724a9adfff8a3677626925024ba233403a406
Michael Bottom <mlbmike@gmail.com>
2023-03-15 20:29:33 -0700

    Update A5 project 4.cpp

--- A5/A5 project 4.cpp
+++ A5/A5 project 4.cpp
@@ -16,204 +16,239 @@ sold for that stand.*/
 #include <string>
 using namespace std;
 
+// Define the HotDogStand class
 class HotDogStand
 {
 public:
-	HotDogStand(int standIdValue, int hotDogsSoldValue);
-	void justSold();
-	int totalSold();
-	static int allIdSold();
+    // Constructor
+    HotDogStand(int standIdValue, int hotDogsSoldValue);
+
+    // Member functions
+    void justSold();
+    int totalSold();
+    static int allIdSold();
 
 private:
-	int standId;
-	int hotDogsSold;
-	static int allIdHotDogsSold;
+    // Private data members
+    int standId;
+    int hotDogsSold;
+    static int allIdHotDogsSold;
 };
 
+// Initialize the static data member
 int HotDogStand::allIdHotDogsSold = 0;
 
 int main()
 {
-	char userChoiceMainMenu = 'N';
-	int userChoiceStand = 0;
-	int newSalesAmount = 0;
-	int newSalesSpecialChoice = 0;
-	HotDogStand michiganAve(0001, 0);
-	HotDogStand magnificentMile(0002, 0);
-	HotDogStand lakeShoreDrive(0003, 0);
+    // Variable declarations
+    char userChoiceMainMenu = 'N';
+    int userChoiceStand = 0;
+    int newSalesAmount = 0;
+    int newSalesSpecialChoice = 0;
 
-	while (userChoiceMainMenu != 'E')
-	{
-		cout << "Current Hot Dog Stands are: \n 1. Michigan Ave \n 2. Magnificent Mile \n 3. Lake Shore Drive.\n";
-		cout << "Enter the choice for stand number: ";
-		cin >> userChoiceStand;
+    // Create HotDogStand objects
+    HotDogStand michiganAve(0001, 0);
+    HotDogStand magnificentMile(0002, 0);
+    HotDogStand lakeShoreDrive(0003, 0);
 
-		if (userChoiceStand == 1)
-		{
-			cout << "Michigan Ave Hot Dog Stand's number of hot dogs sold is: " << michiganAve.totalSold() << endl;
-			cout << "Enter new of new sales: \n";
-			cin >> newSalesAmount;
-			int i;
-			for (i = 0; i < newSalesAmount; i++)
-			{
-				cout << " 1. Chipotle Hot Dog\n 2. Chili Cheese Hot Dog\n 3. Mac and Cheese Hot Dog\n"
-					<< " 4. Double Buffalo Hot Dogs\n 5. Onion and Bacon Hot Dog\n 6. Family Meal Hot Dogs Special\n";
-				cout << "Enter sold sale special number: \n";
-				cin >> newSalesSpecialChoice;
-				if (newSalesSpecialChoice == 1)
-				{
-					michiganAve.justSold();
-				}
-				else if (newSalesSpecialChoice == 2)
-				{
-					michiganAve.justSold();
-				}
-				else if (newSalesSpecialChoice == 3)
-				{
-					michiganAve.justSold();
-				}
-				else if (newSalesSpecialChoice == 4)
-				{
-					michiganAve.justSold();
-					michiganAve.justSold();
-				}
-				else if (newSalesSpecialChoice == 5)
-				{
-					michiganAve.justSold();
-				}
-				else if (newSalesSpecialChoice == 6)
-				{
-					int counter = 0;
-					while (counter < 4)
-					{
-						michiganAve.justSold();
-						counter++;
-					}
-				}
-			}
-			cout << "Michigan Ave Hot Dog Stand's number of hot dogs sold is: " << michiganAve.totalSold() << endl;
-		}
-		else if (userChoiceStand == 2)
-		{
-			cout << "Magnificent Mile Hot Dog Stand's number of hot dogs sold is: " << magnificentMile.totalSold() << endl;
-			cout << "Enter new of new sales: \n";
-			cin >> newSalesAmount;
-			int i;
-			for (i = 0; i < newSalesAmount; i++)
-			{
-				cout << " 1. Chipotle Hot Dog\n 2. Chili Cheese Hot Dog\n 3. Mac and Cheese Hot Dog\n"
-					<< " 4. Double Buffalo Hot Dogs\n 5. Onion and Bacon Hot Dog\n 6. Family Meal Hots Dog Special\n";
-				cout << "Enter sold sale special number: \n";
-				cin >> newSalesSpecialChoice;
-				if (newSalesSpecialChoice == 1)
-				{
-					magnificentMile.justSold();
-				}
-				else if (newSalesSpecialChoice == 2)
-				{
-					magnificentMile.justSold();
-				}
-				else if (newSalesSpecialChoice == 3)
-				{
-					magnificentMile.justSold();
-				}
-				else if (newSalesSpecialChoice == 4)
-				{
-					magnificentMile.justSold();
-					magnificentMile.justSold();
-				}
-				else if (newSalesSpecialChoice == 5)
-				{
-					magnificentMile.justSold();
-				}
-				else if (newSalesSpecialChoice == 6)
-				{
-					int counter = 0;
-					while (counter < 4)
-					{
-						magnificentMile.justSold();
-						counter++;
-					}
-				}
-			}
-			cout << "Magnificent Mile Hot Dog Stand's number of hot dogs sold is: " << magnificentMile.totalSold() << endl;
-		}
-		else
-		{
-			cout << "Lake Shore Drive Hot Dog Stand's number of hot dogs sold is: " << lakeShoreDrive.totalSold() << endl;
-			cout << "Enter new of new sales: \n";
-			cin >> newSalesAmount;
-			int i;
-			for (i = 0; i < newSalesAmount; i++)
-			{
-				cout << " 1. Chipotle Hot Dog\n 2. Chili Cheese Hot Dog\n 3. Mac and Cheese Hot Dog\n"
-					<< " 4. Double Buffalo Hot Dogs\n 5. Onion and Bacon Hot Dog\n 6. Family Meal Hot Dogs Special\n";
-				cout << "Enter sold sale special number: \n";
-				cin >> newSalesSpecialChoice;
-				if (newSalesSpecialChoice == 1)
-				{
-					lakeShoreDrive.justSold();
-				}
-				else if (newSalesSpecialChoice == 2)
-				{
-					lakeShoreDrive.justSold();
-				}
-				else if (newSalesSpecialChoice == 3)
-				{
-					lakeShoreDrive.justSold();
-				}
-				else if (newSalesSpecialChoice == 4)
-				{
-					lakeShoreDrive.justSold();
-					lakeShoreDrive.justSold();
-				}
-				else if (newSalesSpecialChoice == 5)
-				{
-					lakeShoreDrive.justSold();
-				}
-				else if (newSalesSpecialChoice == 6)
-				{
-					int counter = 0;
-					while (counter < 4)
-					{
-						lakeShoreDrive.justSold();
-						counter++;
-					}
-				}
-			}
-			cout << "Lake Shore Drive Hot Dog Stand's number of hot dogs sold is: " << lakeShoreDrive.totalSold() << endl;
-		}
-		cout << "Enter 'E' to exit or enter any other letter to continue: ";
-		cin >> userChoiceMainMenu;
+    // Main loop
+    while (userChoiceMainMenu != 'E')
+    {
+        // Display hot dog stands and prompt user for choice
+        cout << "Current Hot Dog Stands are: \n 1. Michigan Ave \n 2. Magnificent Mile \n 3. Lake Shore Drive.\n";
+        cout << "Enter the choice for stand number: ";
+        cin >> userChoiceStand;
 
-	}
+        // Michigan Ave stand
+        if (userChoiceStand == 1)
+        {
+            // Display current sales and prompt for new sales
+            cout << "Michigan Ave Hot Dog Stand's number of hot dogs sold is: " << michiganAve.totalSold() << endl;
+            cout << "Enter new of new sales: \n";
+            cin >> newSalesAmount;
 
+            // Process new sales for Michigan Ave stand
+            int i;
+            for (i = 0; i < newSalesAmount; i++)
+            {
+                // Display menu and prompt for sold sale special number
+                cout << " 1. Chipotle Hot Dog\n 2. Chili Cheese Hot Dog\n 3. Mac and Cheese Hot Dog\n"
+                     << " 4. Double Buffalo Hot Dogs\n 5. Onion and Bacon Hot Dog\n 6. Family Meal Hot Dogs Special\n";
+                cout << "Enter sold sale special number: \n";
+                cin >> newSalesSpecialChoice;
 
+                // Update sales based on user's choice
+                if (newSalesSpecialChoice == 1)
+                {
+                    michiganAve.justSold();
+                }
+                else if (newSalesSpecialChoice == 2)
+                {
+                    michiganAve.justSold();
+                }
+                else if (newSalesSpecialChoice == 3)
+                {
+                    michiganAve.justSold();
+                }
+                else if (newSalesSpecialChoice == 4)
+                {
+                    michiganAve.justSold();
+                    michiganAve.justSold();
+                }
+                else if (newSalesSpecialChoice == 5)
+                {
+                    michiganAve.justSold();
+                }
+                else if (newSalesSpecialChoice == 6)
+                {
+                    int counter = 0;
+                    while (counter < 4)
+                    {
+                        michiganAve.justSold();
+                        counter++;
+                    }
+                }
+            }
 
+            // Display updated sales for Michigan Ave stand
+            cout << "Michigan Ave Hot Dog Stand's number of hot dogs sold is: " << michiganAve.totalSold() << endl;
+        }
+        // Magnificent Mile
+        else if (userChoiceStand == 2)
+            {
+            // Display current sales and prompt for new sales
+            cout << "Magnificent Mile Hot Dog Stand's number of hot dogs sold is: " << magnificentMile.totalSold() << endl;
+            cout << "Enter new of new sales: \n";
+            cin >> newSalesAmount;
+                    // Process new sales for Magnificent Mile stand
+            int i;
+            for (i = 0; i < newSalesAmount; i++)
+            {
+                // Display menu and prompt for sold sale special number
+                cout << " 1. Chipotle Hot Dog\n 2. Chili Cheese Hot Dog\n 3. Mac and Cheese Hot Dog\n"
+                    << " 4. Double Buffalo Hot Dogs\n 5. Onion and Bacon Hot Dog\n 6. Family Meal Hot Dogs Special\n";
+                cout << "Enter sold sale special number: \n";
+                cin >> newSalesSpecialChoice;
 
-	return 0;
+                // Update sales based on user's choice
+                if (newSalesSpecialChoice == 1)
+                {
+                    magnificentMile.justSold();
+                }
+                else if (newSalesSpecialChoice == 2)
+                {
+                    magnificentMile.justSold();
+                }
+                else if (newSalesSpecialChoice == 3)
+                {
+                    magnificentMile.justSold();
+                }
+                else if (newSalesSpecialChoice == 4)
+                {
+                    magnificentMile.justSold();
+                    magnificentMile.justSold();
+                }
+                else if (newSalesSpecialChoice == 5)
+                {
+                    magnificentMile.justSold();
+                }
+                else if (newSalesSpecialChoice == 6)
+                {
+                    int counter = 0;
+                    while (counter < 4)
+                    {
+                        magnificentMile.justSold();
+                        counter++;
+                    }
+                }
+            }
+
+            // Display updated sales for Magnificent Mile stand
+            cout << "Magnificent Mile Hot Dog Stand's number of hot dogs sold is: " << magnificentMile.totalSold() << endl;
+        }
+        // Lake Shore Drive stand
+        else
+        {
+            // Display current sales and prompt for new sales
+            cout << "Lake Shore Drive Hot Dog Stand's number of hot dogs sold is: " << lakeShoreDrive.totalSold() << endl;
+            cout << "Enter new of new sales: \n";
+            cin >> newSalesAmount;
+
+            // Process new sales for Lake Shore Drive stand
+            int i;
+            for (i = 0; i < newSalesAmount; i++)
+            {
+                // Display menu and prompt for sold sale special number
+                cout << " 1. Chipotle Hot Dog\n 2. Chili Cheese Hot Dog\n 3. Mac and Cheese Hot Dog\n"
+                    << " 4. Double Buffalo Hot Dogs\n 5. Onion and Bacon Hot Dog\n 6. Family Meal Hot Dogs Special\n";
+                cout << "Enter sold sale special number: \n";
+                cin >> newSalesSpecialChoice;
+
+                // Update sales based on user's choice
+                if (newSalesSpecialChoice == 1)
+                {
+                    lakeShoreDrive.justSold();
+                }
+                else if (newSalesSpecialChoice == 2)
+                {
+                    lakeShoreDrive.justSold();
+                }
+                else if (newSalesSpecialChoice == 3)
+                {
+                    lakeShoreDrive.justSold();
+                }
+                else if (newSalesSpecialChoice == 4)
+                {
+                    lakeShoreDrive.justSold();
+                    lakeShoreDrive.justSold();
+                }
+                else if (newSalesSpecialChoice == 5)
+                {
+                    lakeShoreDrive.justSold();
+                }
+                else if (newSalesSpecialChoice == 6)
+                {
+                    int counter = 0;
+                    while (counter < 4)
+                    {
+                        lakeShoreDrive.justSold();
+                        counter++;
+                    }
+                }
+            }
+
+            // Display updated sales for Lake Shore Drive stand
+            cout << "Lake Shore Drive Hot Dog Stand's number of hot dogs sold is: " << lakeShoreDrive.totalSold() << endl;
+        }
+
+        // Prompt user to exit or continue
+        cout << "Enter 'E' to exit or enter any other letter to continue: ";
+        cin >> userChoiceMainMenu;
+    }
+
+    // End the program
+    return 0;
 }
 
-
-
+// HotDogStand class constructor
 HotDogStand::HotDogStand(int standIdValue, int hotDogsSoldValue)
-{
-	standId = standIdValue;
-	hotDogsSold = hotDogsSoldValue;
-}
+    {
+        standId = standIdValue;
+        hotDogsSold = hotDogsSoldValue;
+    }
 
+// HotDogStand class member functions
 void HotDogStand::justSold()
-{
-	hotDogsSold += 1;
-}
+    {
+        hotDogsSold += 1;
+    }
 
 int HotDogStand::totalSold()
-{
-	return hotDogsSold;
-}
+    {
+        return hotDogsSold;
+    }
 
 int HotDogStand::allIdSold()
-{
-	return allIdHotDogsSold;
-}
-
+    {
+        return allIdHotDogsSold;
+    }
