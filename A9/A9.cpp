337001ee41bf1983311f7811981077eae50666b9
Michael Bottom <mlbmike@gmail.com>
2023-03-29 19:06:50 -0700

    Update A9.cpp

--- A9/A9.cpp
+++ A9/A9.cpp
@@ -10,67 +10,64 @@ supervisor of the administor, to enter data of the administrator, and to read th
 the administrator. There is also an overloaded function to print a check for the 
 administrator.*/
 
-#include "stdafx.h"
 #include <iostream>
-#include <cstdlib>
 #include <string>
-
-
 using namespace std;
 
 class Employee {
 public:
-	Employee();
-	Employee(const string& theName, const string& theSsn);
-	string getName() const;
-	string getSsn() const;
-	double getNetPay() const;
-	void setName(const string& newName);
-	void setSsn(const string& newSsn);
-	void setNetPay(double newNetPay);
-	void printCheck() const;
+    Employee();
+    Employee(const string& theName, const string& theSsn);
+    string getName() const;
+    string getSsn() const;
+    double getNetPay() const;
+    void setName(const string& newName);
+    void setSsn(const string& newSsn);
+    void setNetPay(double newNetPay);
+    void printCheck() const;
 
 private:
-	string name;
-	string ssn;
-	double netPay;
+    string name;
+    string ssn;
+    double netPay;
 };
 
 class SalariedEmployee : public Employee {
 public:
-	SalariedEmployee();
-	SalariedEmployee(const string& theName, const string& theSsn,
-						double theWeeklySalary);
-	double getSalary() const;
-	void setSalary(double newSalary);
-	void printCheck();
+    SalariedEmployee();
+    SalariedEmployee(const string& theName, const string& theSsn,
+        double theWeeklySalary);
+    double getSalary() const;
+    void setSalary(double newSalary);
+    void printCheck();
 
 protected:
-	double salary; //weekly
+    double salary; //weekly
 
 };
 
 class Administrator : public SalariedEmployee {
 public:
-	Administrator(const string& theName, const string& theSsn,
-					double theAnnualSalary);
-	void setSupervisor(const string& newName);
-	void dataEntry();
-	void print();
-	void printCheck();
-	Administrator();
+    Administrator(const string& theName, const string& theSsn,
+        double theAnnualSalary);
+    void setSupervisor(const string& newName);
+    void dataEntry();
+    void print();
+    void printCheck();
+    Administrator();
 
 
 protected:
-	double salaryAnnualAdmin;
+    double salaryAnnualAdmin;
 
 
 private:
-	string titleAdmin;
-	string compAreaResponsibility;
-	string supervisorOfAdmin;
+    string titleAdmin;
+    string compAreaResponsibility;
+    string supervisorOfAdmin;
 };
 
+// Employee class implementation
 Employee::Employee() : name("None"), ssn("None"), netPay(0) {
 
 }
@@ -81,97 +78,107 @@ ssn(theNumber), netPay(0) {
 }
 
 string Employee::getName() const {
-	return name;
+    return name;
 }
 
 string Employee::getSsn() const {
-	return ssn;
+    return ssn;
 }
 
 double Employee::getNetPay() const {
-	return netPay;
+    return netPay;
 }
 
 void Employee::setName(const string& newName) {
-	name = newName;
+    name = newName;
 }
 
 void Employee::setSsn(const string& newSsn) {
-	ssn = newSsn;
+    ssn = newSsn;
 }
 
 void Employee::setNetPay(double newNetPay) {
-	netPay = newNetPay;
+    netPay = newNetPay;
 }
 
+// SalariedEmployee class implementation
 SalariedEmployee::SalariedEmployee() : Employee(), salary(0) {
 
 }
 
 SalariedEmployee::SalariedEmployee(const string& theName, const string& theNumber,
-	double theWeeklyPay) : Employee(theName, theNumber), salary(theWeeklyPay) {
+    double theWeeklyPay) : Employee(theName, theNumber), salary(theWeeklyPay) {
 
 }
 
+double SalariedEmployee::getSalary() const {
+    return salary;
+}
 
+void SalariedEmployee::setSalary(double newSalary) {
+    salary = newSalary;
+}
 
+void SalariedEmployee::printCheck() {
+    setNetPay(salary);
+    cout << "\n________________________________________________\n";
+    cout << "Pay to the order of " << getName() << endl;
+    cout << "The sum of " << getNetPay() << " Dollars\n";
+    cout << "_________________________________________________\n";
+    cout << "Check Stub NOT NEGOTIABLE \n";
+    cout << "Administrator Number: " << getSsn() << endl;
+}
+
+// Administrator class implementation
 Administrator::Administrator() : SalariedEmployee(), titleAdmin("None"),
-	compAreaResponsibility("None"), supervisorOfAdmin("None") {
+    compAreaResponsibility("None"), supervisorOfAdmin("None") {
 
 }
 
-Administrator::Administrator(const string& theName, const string& theSsn, double theAnnualSalary) : 
-	SalariedEmployee(theName, theSsn, theAnnualSalary), titleAdmin("None"),
-	compAreaResponsibility("None"), supervisorOfAdmin("None") {
-	
+Administrator::Administrator(const string& theName, const string& theSsn, double theAnnualSalary) :
+    SalariedEmployee(theName, theSsn, theAnnualSalary), titleAdmin("None"),
+    compAreaResponsibility("None"), supervisorOfAdmin("None") {
+
 }
 
 void Administrator::setSupervisor(const string& newName) {
-	supervisorOfAdmin = newName;
+    supervisorOfAdmin = newName;
 }
 
 void Administrator::dataEntry() {
-	cout << endl << "Data Entry" << endl;
-	cout << endl << "What is the title of the Administrator: " << endl;
-	getline(cin, titleAdmin);
-	cout << endl << "What is the company's area of responsibility: " << endl;
-	getline(cin, compAreaResponsibility);
-	cout << endl << "What is the name of the Administrator's supervisor: " << endl;
-	getline(cin, supervisorOfAdmin);
+    cout << endl << "Data Entry
+    cout << endl << "What is the title of the Administrator: " << endl;
+    getline(cin, titleAdmin);
+    cout << endl << "What is the company's area of responsibility: " << endl;
+    getline(cin, compAreaResponsibility);
+    cout << endl << "What is the name of the Administrator's supervisor: " << endl;
+    getline(cin, supervisorOfAdmin);
 }
 
 void Administrator::print() {
-	cout << endl << "The Administrator's name is: " << getName();
-	cout << endl << "The Administrator's title is: " << titleAdmin;
-	cout << endl << "The company's area of responsibility is: " << compAreaResponsibility;
-	cout << endl << "The Administrator's supervisor is: " << supervisorOfAdmin;
-	cout << endl;
-
+    cout << endl << "The Administrator's name is: " << getName();
+    cout << endl << "The Administrator's title is: " << titleAdmin;
+    cout << endl << "The company's area of responsibility is: " << compAreaResponsibility;
+    cout << endl << "The Administrator's supervisor is: " << supervisorOfAdmin;
+    cout << endl;
 }
 
-
-
-
 void Administrator::printCheck() {
-	setNetPay(salary);
-	cout << "\n________________________________________________\n";
-	cout << "Pay to the order of " << getName() << endl;
-	cout << "The sum of " << getNetPay() << " Dollars\n";
-	cout << "_________________________________________________\n";
-	cout << "Check Stub NOT NEGOTIABLE \n";
-	cout << "Administrator Number: " << getSsn() << endl;
-
+    setNetPay(getSalary() / 52.0);
+    cout << "\n________________________________________________\n";
+    cout << "Pay to the order of " << getName() << endl;
+    cout << "The sum of " << getNetPay() << " Dollars\n";
+    cout << "_________________________________________________\n";
+    cout << "Check Stub NOT NEGOTIABLE \n";
+    cout << "Administrator Number: " << getSsn() << endl;
 }
 
 int main() {
-	cout << "Entering data for a new Administrator...";
-	Administrator Louis("Louis Watson", "455760132", 45000);
-	Louis.printCheck();
-	Louis.dataEntry();
-	Louis.print();
-	Louis.printCheck();
-
-	
-	return 0;
+    cout << "Entering data for a new Administrator...";
+    Administrator Louis("Louis Watson", "455760132", 45000);
+    Louis.printCheck();
+    Louis.dataEntry();
+    Louis.print();
+    Louis.printCheck();
+    return 0;
 }
-
